a63a6903eb632057fd00b6fc395c74f09c38f5fd
Estrella Mooney <estrellamooney96@gmail.com>
2023-08-01 13:49:41 +1200

    Complete

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ import reportWebVitals from './reportWebVitals'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+// reportWebVitals(console.log))
+reportWebVitals()

8bb9f9d9502a088138713d4b1a4555c7f0b1db8b
Estrella Mooney <estrellamooney96@gmail.com>
2023-08-01 13:42:19 +1200

    Create app func --> add consts

--- src/App.js
+++ src/App.js
@@ -2,3 +2,16 @@ import React, { useEffect, useState } from 'react'
 import Start from './components/Start'
 import Quiz from './components/Quiz'
 import Result from './components/Result'
+
+function App() {
+  const [quizs, setQuizs] = useState([])
+  const [question, setQuesion] = useState({})
+  const [questionIndex, setQuestionIndex] = useState(0)
+  const [correctAnswer, setCorrectAnswer] = useState('')
+  const [selectedAnswer, setSelectedAnswer] = useState('')
+  const [marks, setMarks] = useState(0)
+
+  return <></>
+}
+
+export default App

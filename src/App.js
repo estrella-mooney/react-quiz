c92d838c0efbd2783ca2091c3dd446e5103366cd
Estrella Mooney <estrellamooney96@gmail.com>
2023-08-03 11:25:23 +1200

    Working

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Quiz from './components/Quiz'
 import Result from './components/Result'
 
 function App() {
+  // All Quizs, Current Question, Index of Current Question, Answer, Selected Answer, Total Marks
   const [quizs, setQuizs] = useState([])
   const [question, setQuesion] = useState({})
   const [questionIndex, setQuestionIndex] = useState(0)
@@ -23,12 +24,94 @@ function App() {
       .then((data) => setQuizs(data))
   }, [])
 
+  // Set a Single Question
+  useEffect(() => {
+    if (quizs.length > questionIndex) {
+      setQuesion(quizs[questionIndex])
+    }
+  }, [quizs, questionIndex])
+
+  // Start Quiz
   const startQuiz = () => {
     setShowStart(false)
     setShowQuiz(true)
   }
 
-  return <></>
+  // Check Answer
+  const checkAnswer = (event, selected) => {
+    if (!selectedAnswer) {
+      setCorrectAnswer(question.answer)
+      setSelectedAnswer(selected)
+
+      if (selected === question.answer) {
+        event.target.classList.add('bg-success')
+        setMarks(marks + 5)
+      } else {
+        event.target.classList.add('bg-danger')
+      }
+    }
+  }
+
+  // Next Quesion
+  const nextQuestion = () => {
+    setCorrectAnswer('')
+    setSelectedAnswer('')
+    const wrongBtn = document.querySelector('button.bg-danger')
+    wrongBtn?.classList.remove('bg-danger')
+    const rightBtn = document.querySelector('button.bg-success')
+    rightBtn?.classList.remove('bg-success')
+    setQuestionIndex(questionIndex + 1)
+  }
+
+  // Show Result
+  const showTheResult = () => {
+    setShowResult(true)
+    setShowStart(false)
+    setShowQuiz(false)
+  }
+
+  // Start Over
+  const startOver = () => {
+    setShowStart(false)
+    setShowResult(false)
+    setShowQuiz(true)
+    setCorrectAnswer('')
+    setSelectedAnswer('')
+    setQuestionIndex(0)
+    setMarks(0)
+    const wrongBtn = document.querySelector('button.bg-danger')
+    wrongBtn?.classList.remove('bg-danger')
+    const rightBtn = document.querySelector('button.bg-success')
+    rightBtn?.classList.remove('bg-success')
+  }
+
+  return (
+    <>
+      {/* Welcome Page */}
+      <Start startQuiz={startQuiz} showStart={showStart} />
+
+      {/* Quiz Page */}
+      <Quiz
+        showQuiz={showQuiz}
+        question={question}
+        quizs={quizs}
+        checkAnswer={checkAnswer}
+        correctAnswer={correctAnswer}
+        selectedAnswer={selectedAnswer}
+        questionIndex={questionIndex}
+        nextQuestion={nextQuestion}
+        showTheResult={showTheResult}
+      />
+
+      {/* Result Page */}
+      <Result
+        showResult={showResult}
+        quizs={quizs}
+        marks={marks}
+        startOver={startOver}
+      />
+    </>
+  )
 }
 
 export default App
